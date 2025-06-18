85abe6fde6849c7dfb72c032d3153225026f1267
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-06-17 20:08:27 -0400

    update

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,12 +1,15 @@
 
-export default function DetailPost({
+export default async function DetailPost({
     params
 }: {
     params: Promise<{ id: string }>
 }) {
+
+    const { id } = await params;
+
     return (
         <div className="flex flex-col items-center justify-center">
-            <h1 className="text-center font-bold mt-9 text-6xl">Post</h1>
+            <h1 className="text-center font-bold mt-9 text-6xl">Detalhes do Post: {id}</h1>
         </div>
     );
 }
